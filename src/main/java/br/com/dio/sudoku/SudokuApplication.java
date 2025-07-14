8573fe8aca60037c0a3ccb106b7d20207d1e8721
Tavaressan <vitortctavares@gmail.com>
2025-07-14 16:15:43 -0300

    fix: Modificados nomes das classes para legibilidade

--- src/main/java/br/com/dio/sudoku/SudokuApplication.java
+++ src/main/java/br/com/dio/sudoku/SudokuApplication.java
@@ -8,10 +8,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 // Esta classe executa a aplicação com a GUI Java FX
-public class HelloApplication extends Application {
+public class SudokuApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SudokuApplication.class.getResource("sudoku-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 450, 500);
 
         // Carrega a folha de estilos CSS
--- src/main/java/br/com/dio/sudoku/SudokuController.java
+++ src/main/java/br/com/dio/sudoku/SudokuController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.function.UnaryOperator;
 
-public class HelloController {
+public class SudokuController {
 
     // Injeção dos componentes do FXML
     @FXML
